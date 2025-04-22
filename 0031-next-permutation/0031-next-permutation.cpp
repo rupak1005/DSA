0bee4fc336fbf16336140c84485897bb2eb56540
Rupakkumar <81298410+rupak1005@users.noreply.github.com>
2025-04-22 19:01:02 +0530

    0031-NEXT-PERMUTATION Stats: Time: 0 ms (100%), Space: 15.6 MB (92.35%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int arr[n];
-        int j=0;
-        for(auto i:nums){
-            arr[j++]=i;
-
-        }
         
-        next_permutation(arr,arr+n);
-        j=0;
-        for(auto i:arr){
-            nums[j++]=i;
-        }
+        
+        next_permutation(nums.begin(),nums.end());
         
         
     }
