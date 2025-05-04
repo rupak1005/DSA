579abce833f78a7c84f1a990c5863a172ca9906f
rupak1005 <rupak1005saini@gmail.com>
2025-05-04 10:53:32 +0530

    maxSUbarray print

--- STRIVERS SHEET/kedane'salgo.cpp
+++ STRIVERS SHEET/kedane'salgo.cpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 void maxSubarray(vector<int> nums){
-    int maxsum=INT_MIN;
-    int currsum=0;
+    int maxsum=nums[0];
+    int currsum=nums[0];
     int n=nums.size();
     int s=0;
     int e=0;
     int tempstart=0;
     
-    for(int i=0;i<n;i++){
-        currsum+=nums[i];
-        if(currsum>maxsum){
-            maxsum=currsum;
-            s=tempstart;
-            e=i;
-        }
+    for(int i=1;i<n;i++){
+       currsum+=nums[i];
+       if(currsum>maxsum){
+           maxsum=currsum;
+           s=tempstart;
+           e=i;
+       }
+    
         if(currsum<0){
             currsum=0;
             tempstart=i+1;
