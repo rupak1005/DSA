c1e3acf6b60b8168bfea6f4ab4ab62b00e45d3bf
rupak1005 <rupak1005saini@gmail.com>
2025-05-07 22:14:01 +0530

    majority el n/3

--- STRIVERS SHEET/ARRAY QUESTIONS/moore'sVoting.cpp
+++ STRIVERS SHEET/ARRAY QUESTIONS/moore'sVoting.cpp
@@ -1,5 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = 0, el2 = 1;  // initialize to different values
+        
+        for (auto i : nums) {
+            if (el1 == i) cnt1++;
+            else if (el2 == i) cnt2++;
+            else if (cnt1 == 0) {
+                el1 = i;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                el2 = i;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        // Verify counts
+        cnt1 = cnt2 = 0;
+        for (auto i : nums) {
+            if (i == el1) cnt1++;
+            else if (i == el2) cnt2++;
+        }
+        
+        if (cnt1 > n / 3) ans.push_back(el1);
+        if (cnt2 > n / 3 && el2 != el1) ans.push_back(el2);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+    
 
 int majorityelement(vector<int>&nums){
     int cnt=0;
@@ -26,7 +61,9 @@ int majorityelement(vector<int>&nums){
     return -1;
 }
 int main(){
-    vector<int> nums={1,2,3,5,5,5,5,5};
-    int n=majorityelement(nums);
-    cout<<n;
+    vector<int> nums={1,2};
+    vector<int> ans=majorityElement(nums);
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
 }
