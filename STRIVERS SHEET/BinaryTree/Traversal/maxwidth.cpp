1c4cf5f80d8629a5b6be568d5f0eb4a1260bbabd
Rupak Saini <rupak1005saini@gmail.com>
2025-05-29 09:05:06 +0530

    max width

---
+++ STRIVERS SHEET/BinaryTree/Traversal/maxwidth.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int ans = 0;
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            long long size = q.size();
+            long long mmin = q.front().second;
+            long long first, last;
+            for (int i = 0; i < size; i++) {
+                long long curr_ind = q.front().second - mmin;
+                TreeNode* node = q.front().first;
+                q.pop();
+                if (i == 0)
+                    first = curr_ind;
+                if (i == size - 1)
+                    last = curr_ind;
+                if (node->left) {
+                    q.push({node->left, curr_ind * 2 + 1});
+                }
+                if (node->right) {
+                    q.push({node->right, curr_ind * 2 + 2});
+                }
+            }
+            ans = max(ans, static_cast<int>(last - first + 1));
+        }
+        return ans;
+    }
+};
